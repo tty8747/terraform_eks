3a6a69c615c482f2d598cbc7424b3fe7fc30af40
Sergey Chulanov <tty8747@gmail.com>
2024-10-26 14:13:12 +0300

    Add networks into output

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -7,3 +7,12 @@ output "cidr" {
   value       = aws_vpc.vpc.cidr_block
   description = "The CIDR block assigned to the main VPC"
 }
+
+output "pub_subnets" {
+  value = aws_subnet.public
+}
+
+output "priv_subnets" {
+  value = aws_subnet.private
+}
+
